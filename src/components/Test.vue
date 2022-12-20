9efa7b30244eb01a9519bb2ece91495193c23964
zhwiii <1449027831@qq.com>
2022-12-20 19:01:02 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import api from "../api";
+export default {
+  name: "test",
+  data(){
+    return{
+      msg:''
+    }
+  },
+  mounted () {
+    this.getMsg()
+  },
+  methods:{
+    getMsg () {
+      let funcName = 'getMsg'
+      api[funcName]().then(res => {
+        this.msg=res
+      })
+      },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
